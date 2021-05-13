18427b222403e78dfe1e909477b5fa4c43b4d578
schilakala <Sandeep_Chilakala@intuit.com>
2021-05-13 16:51:35 +0530

    Error

--- Errors/Errors.go
+++ Errors/Errors.go
@@ -3,7 +3,17 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 )
+
+type argError struct {
+	arg  int
+	prob string
+}
+
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
 
 func f1(arg int) (int, error) {
 	if arg == 42 {
@@ -15,15 +25,6 @@ func f1(arg int) (int, error) {
 	return arg + 3, nil
 }
 
-type argError struct {
-	arg  int
-	prob string
-}
-
-func (e *argError) Error() string {
-	return fmt.Sprintf("%d - %s", e.arg, e.prob)
-}
-
 func f2(arg int) (int, error) {
 	if arg == 42 {
 
@@ -32,8 +33,44 @@ func f2(arg int) (int, error) {
 	return arg + 3, nil
 }
 
+//my code start
+type customError struct {
+	start  int
+	end    int
+	errStr string
+}
+
+func (e *customError) Error() string {
+	return fmt.Sprintf("Your start number %d should be less than end number %d - %s", e.start, e.end, e.errStr)
+}
+
+func sumOfNumbers(start, end int) (int, error) {
+	if start > end {
+		return -1, errors.New("Start should be less than end")
+	} else if end == 0 {
+		return -1, &customError{start, end, "custom Error"}
+	}
+	sum := 0
+	for i := start; i < end; i++ {
+		sum += i
+	}
+	return sum, nil
+}
+
+//my code end
+
 func main() {
 
+	//my code start
+	log.Println(sumOfNumbers(10, 8))
+	//get each value from the custom error struct
+	_, er := sumOfNumbers(-1, 0)
+	if ce, ok := er.(*customError); ok { // this to instantate the error object to get the values in it
+		log.Println(ce.start)
+		log.Println(ce.end)
+		log.Println(ce.errStr)
+	}
+	//my code end
 	for _, i := range []int{7, 42} {
 		if r, e := f1(i); e != nil {
 			fmt.Println("f1 failed:", e)
